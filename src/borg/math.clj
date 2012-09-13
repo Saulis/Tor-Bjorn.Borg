1a4b62156e1d5a5bee13a6a6b5ba5a3b9a18293a
Sauli Tähkäpää <sauli.tahkapaa@netti.fi>
2012-09-13 21:23:19 +0300

    Trying things with math.

--- src/borg/math.clj
+++ src/borg/math.clj
@@ -4,6 +4,41 @@
 
 (defstruct point :x :y)
 
+(defn x-delta [p1 p2]
+  (- (:x p1) (:x p2)))
+
+(defn y-delta [p1 p2]
+  (- (:y p1) (:y p2)))
+
+(defn slope [p1 p2]
+  (/ (y-delta p1 p2) (x-delta p1 p2)))
+
+(defn distance-to-left-hit [x]
+  (- (left-hit-width) x))
+
+(defn distance-to-top-hit [y]
+  (- (top-hit-height y)))
+
+(defn distance-to-bottom-hit [y]
+  (- (bottom-hit-height y)))
+
+(defn hit-height-on-left [x y slope]
+  (+ (* slope (distance-to-left-hit x)) y))
+
+(defn hit-width [x distance-to-y slope]
+  (+ (/ distance-to-y slope) x))
+
+(defn hit-width-on-top [x y slope]
+  (hit-width x (distance-to-top-hit y) slope))
+
+(defn hit-width-on-bottom [x y slope]
+  (hit-width x (distance-to-bottom-hit y) slope))
+
+
+
+
+
+
 (defn to-point [x y]
   (struct point x y))
 
@@ -36,3 +71,81 @@
 
 
 
+(defn negative-slope [p1 p2]
+  (* -1 (slope p1 p2)))
+
+(defn impact-height-on-left-wall [p1 p2]
+  (+ (* (slope p1 p2) (- 0 (:x p2))) (:y p2)))
+
+(defn solve-x [p1 p2 target-y]
+  (+ (/ (- target-y (:y p2)) (slope p1 p2) (:x p2))))
+
+(defn ball-lands-on-left-wall [p1 p2]
+  (let [impact-height (impact-height-on-left-wall p1 p2)]
+  (and
+    (>= 0 impact-height)
+    (<= court-height impact-height))))
+
+(defn landing-point-on-left-wall [p1 p2]
+  (to-point 0 (impact-height-on-left-wall p1 p2)))
+
+(defn ball-will-bounce-via-bottom [p1 p2]
+  (< 0 (impact-height-on-left-wall p1 p2)))
+
+(defn bottom-bounce-position [p1 p2]
+  (to-point (solve-x p1 p2 0) 0))
+
+(defn impact-height-on-left-wall-after-bottom-bounce [p1 p2]
+  (let [bottom-position (bottom-bounce-position p1 p2)]
+      (* (negative-slope p1 p2) (:x bottom-position))))
+
+(defn landing-point-via-bottom-bounce [p1 p2]
+  (to-point 0 (impact-height-on-left-wall-after-bottom-bounce p1 p2)))
+
+;TODO
+(defn landing-point-via-top-bounce [p1 p2]
+  (to-point 0 0))
+
+(defn landing-point-via-bounce [p1 p2]
+  (if (ball-will-bounce-via-bottom p1 p2)
+    (landing-point-via-bottom-bounce p1 p2)
+    (landing-point-via-top-bounce p1 p2)))
+
+(defn landing-point [p1 p2]
+  (if (ball-lands-on-left-wall p1 p2)
+    (landing-point-on-left-wall p1 p2)
+    (landing-point-via-bounce p1 p2)))
+
+(defn down-impact-point [p1 p2]
+  (landing-point (to-point (solve-x p1 p2 0) 0)))
+
+(defn up-impact-point [p1 p2]
+  (landing-point (to-point (solve-x p1 p2 480) 480)))
+
+(defn impact-point [p1 p2]
+  (let [y (impact-height-on-left-wall p1 p2)]
+    (if (<= 0 y)
+      (down-impact-point p1 p2)
+      (up-impact-point p1 p2))))
+
+(defn angle-in-radians [p1 p2]
+  (if
+    (or (nil? (x-delta p1 p2))
+    (= (x-delta p1 p2) 0))
+    0
+  (atan (/ (y-delta p1 p2) (x-delta p1 p2)))))
+
+(defn to-degrees [angle]
+  (toDegrees angle))
+
+(defn angle-in-degrees [p1 p2]
+  (to-degrees (angle-in-radians p1 p2)))
+
+(defn last-two-balls [data]
+  (map :ball (take-last 2 data)))
+
+(defn last-two-positions [data]
+  (map :pos (last-two-balls data)))
+
+(defn angle-from-last-two-positions [data]
+    (angle-in-degrees (second (last-two-positions data)) (first (last-two-positions data))))
--- src/tests/math-tests.clj
+++ src/tests/math-tests.clj
@@ -1,9 +1,30 @@
 (ns tests.math-tests
   (:use clojure.test
+        borg.constants
         borg.math))
 
-(deftest one-hit-impact
-  (testing "one hit impact")
-  (is (= (:x (next-impact-point (to-point 0 0) 45)) 100)))
+(def center (to-point (/ court-width 2) (/ court-height 2)))
+(def right-middle (to-point court-width (/ court-height 2)))
+(def left-middle (to-point 0 (/ court-height 2)))
+(def left-bottom-corner (to-point 0 0))
+(def right-bottom-corner (to-point court-width 0))
+
+(deftest slope-tests
+  (is (= (slope (to-point 5 5) (to-point 5 10)) 666))        ;?
+  (is (= (slope (to-point 0 0) (to-point 5 5 )) 1))
+  (is (= (slope (to-point 0 0) (to-point -5 -5 )) 1))
+  (is (= (slope (to-point 0 0) (to-point -5 5 )) -1)))
+
+(deftest hit-height-on-left-tests
+  (is (= (hit-height-on-left 320 0 -1) 320)))
+
+;(deftest landing-point-tests
+  ;(is (= (landing-point (to-point 640 480) (to-point 320 480)) (to-point 0 480)))
+  ;(is (= (landing-point (to-point 640 0) (to-point 320 0)) (to-point 0 0)))
+  ;(is (= (landing-point (to-point 640 0) (to-point 320 240)) (to-point 0 480)))
+  ;(is (= (landing-point (to-point 640 0) (to-point 160 240)) (to-point 0 0))))
+
+(deftest impact-height-on-left-wall-tests
+  (is (= (impact-height-on-left-wall right-middle center) (:y left-middle))))
 
 (run-all-tests)
