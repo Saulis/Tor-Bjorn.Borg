85ea1dafd15c1b2c1dd3e315b7daa29405523f88
Sauli Tähkäpää <sauli.tahkapaa@netti.fi>
2012-09-16 20:57:51 +0300

    Refactored core and data.

--- src/borg/core.clj
+++ src/borg/core.clj
@@ -12,13 +12,7 @@
            [java.io PrintWriter InputStreamReader BufferedReader])
   (:gen-class :main true))
 
-(def move-up-full-speed {:msgType "changeDir" :data -1.0})
-(def move-up-half-speed {:msgType "changeDir" :data -0.5})
-(def move-down-full-speed {:msgType "changeDir" :data 1.0})
-(def move-down-half-speed {:msgType "changeDir" :data 0.5})
-(def stop {:msgType "changeDir" :data 0})
-
-(defn move [direction]
+(defn move-to [direction]
   {:msgType "changeDir" :data direction})
 
 (defn write [conn data]
@@ -26,108 +20,35 @@
     (.println (json-str data))
     (.flush)))
 
-(defn is-it-time-to-move [{time :time}]
-  (>= (mod time 200) 100))
-
-(defn ball-is-up [{y :y} {ball :pos}]
-  (< y (:y ball)))
-
-(defn to-direction [{left :left ball :ball}]
-  (if (ball-is-up left ball)
-    -1.0
-    1.0))
-
-(defn enough-data-to-move []
-  (>= (count cached-data) 3))
-
-(defn difference-between [target-position current-position]
-  (abs (- target-position current-position)))
-
-(defn move-up-at-speed [target-position current-position]
-  (if (> (difference-between target-position current-position) 5)
-    move-up-full-speed
-    move-up-half-speed))
-
-(defn move-down-at-speed [target-position current-position]
-  (if (> (difference-between target-position current-position) 5)
-    move-down-full-speed
-    move-down-half-speed))
-
-(defn move-at-speed [target-position current-position]
-  (if (> target-position current-position)
-    (move-up-at-speed target-position current-position)
-    (move-down-at-speed target-position current-position)))
-
-
-(defn target-position-is-away [target-position current-position]
-  (>= (difference-between target-position current-position) 5))
-
-(defn move-towards-target-height [target-height current-height]
-  (if (> target-height current-height)
-    move-down-full-speed
-    move-up-full-speed))
-
-;(defn move-towards-target-position [target-position current-position]
-;    (info (str "Current position: " current-position))
-;    (if (target-position-is-away target-position current-position)
-;      (move-at-speed target-position current-position)
-;      stop))
-
 (defn current-height [{left :left}]
   (:y left))
-
-(defn get-target-position [previous-data current-data]
-  )
 
 (defn ball-is-close [ball-position]
   (< (:x ball-position) 50))       ;TODO
 
 (defn foo-target-height []
-  (if (> (count cached-data ) 2)
-    ;(if (ball-is-moving-right (previous-ball-position) (current-ball-position))
-      ;240
-      (if (ball-is-close (current-ball-position))
-      (- (landing-height (previous-ball-position) (current-ball-position)) 50) ;TODO
-      (landing-height (previous-ball-position) (current-ball-position)))
+  (if (> (count saved-data ) 2)
+      (if (ball-is-close (current-ball-position saved-data))
+      (- (landing-height (previous-ball-position saved-data) (current-ball-position saved-data)) 50) ;TODO
+      (landing-height (previous-ball-position saved-data) (current-ball-position saved-data)))
   200))
 
-(defn next-move [data]
-  (move (direction (foo-target-height) (current-height data))))
-
-(defn ball-is-too-close-to-borders [{pos :pos}]
-  (or (<= (:x pos) 50)
-    (<= (:y pos) 50)
-    (>= (:x pos) 600)
-    (>= (:y pos) 430)))
-
-(defn gather-data [data]
-  (info (str "Target position: " target-position ))
-  (update-data data))
-
 (defn new-direction []
-  (direction (foo-target-height) (current-height (last cached-data))))
+  (direction (foo-target-height) (current-height (last saved-data))))
+
+(defn it-is-time-to-change-direction []
+  (and
+    (new-direction-is-accurate (last-three-ball-positions saved-data))
+    (nine-messages-have-not-been-sent-under-one-second saved-messages)))
 
 (defn change-direction [conn]
   (save-message (new-direction))
   (info (str "Direction: " (System/currentTimeMillis) " " (new-direction))) ;TODO debug
-  (write conn (move (new-direction))))
-
-(defn current-direction []
-  (:direction (last-message)))
-
-(defn direction-is-changing []
-  (new-direction-is-accurate (last-three-ball-positions)))
-  ;(if (new-direction-is-accurate (last-three-ball-positions))
-;    (not= (current-direction) (new-direction))
-;    false))
-
-(defn it-is-time-to-change-direction []
-  (and
-    (direction-is-changing)
-    (nine-messages-have-not-been-sent-under-one-second sent-messages)))
+  (write conn (move-to (new-direction))))
 
 (defn handle-data [conn data]
-  (refresh-data data)
+  (info data) ;TODO debug
+  (save-data data)
   (if (it-is-time-to-change-direction)
     (change-direction conn)))
 
--- src/borg/data.clj
+++ src/borg/data.clj
@@ -1,88 +1,36 @@
 (ns borg.data
   (:use borg.math
-        borg.moves
-        [clojure.tools.logging :only (info error)]))
+        borg.moves))
 
 (defstruct time-direction :time :direction)
 
-(def ^:dynamic cached-data [])
-(def ^:dynamic sent-messages [])
-(def ^:dynamic previous-position)
+(def ^:dynamic saved-data [])
+(def ^:dynamic saved-messages [])
 
 (defn clear-data []
-  (def cached-data []))
+  (def saved-data []))
+
+(defn save-data [data]
+  (def saved-data (conj saved-data data)))
 
 (defn clear-messages []
-  (def sent-messages []))
+  (def saved-messages []))
 
 (defn save-message [direction]
-  (def sent-messages (conj sent-messages (struct time-direction (System/currentTimeMillis) direction))))
+  (def saved-messages (conj saved-messages (struct time-direction (System/currentTimeMillis) direction))))
 
-(defn last-message []
-  (last sent-messages))
-
-(defn save-previous-position [data]
-  (def previous-position data))
-
-(defn balls [data]
+(defn- balls [data]
   (map :ball data))
 
-(defn ball-positions []
-  (map :pos (balls cached-data)))
+(defn- ball-positions [data]
+  (map :pos (balls data)))
 
-(defn previous-ball-position []
-  (first (take-last 2 (ball-positions))))
+(defn previous-ball-position [data]
+  (first (take-last 2 (ball-positions data))))
 
-(defn current-ball-position []
-  (last (ball-positions)))
+(defn current-ball-position [data]
+  (last (ball-positions data)))
 
-(defn last-three-ball-positions []
-  (take-last 3 (ball-positions)))
-
-;for debugging
-(defn velocity [p1 p2]
-  (Math/sqrt (+ (Math/pow (x-delta p1 p2) 2) (Math/pow (y-delta p1 p2) 2))))
-
-
-
-(defn refresh-data [data]
-  (info data)
-  ;(info (str "Target: " target-height))
-  ;(if (> (count cached-data) 2)
-    ;(info (str "Velocity: " (velocity (previous-ball-position) (current-ball-position) ))))
-  (def cached-data (conj cached-data data)))
-
-
-
-
-
-(defn ball-x-positions [balls]
-  (map :x (ball-positions balls)))
-
-(defn ball-y-positions [balls]
-  (map :y (ball-positions balls)))
-
-(defn ball-x-delta [previous-ball-position current-ball-position]
-  (- (:x previous-ball-position) (:x current-ball-position)))
-
-(defn ball-is-moving-right [previous-ball-position current-ball-position]
-  (neg? (ball-x-delta previous-ball-position current-ball-position)))
-
-(defn calculate-target-position [data]
-  (info (str "calculating target position"))
-  (info (ball-x-positions (balls data)))
-  (if (ball-is-moving-right (balls data))
-    240
-    (last (ball-y-positions (balls data)))))
-
-(defn update-target-position [data]
-  (def target-position (calculate-target-position data)))
-
-(defn update-data [data]
-  (def cached-data (conj cached-data data))
-  (if (> (count cached-data) 1)
-    (update-target-position cached-data)))
-
-
-
+(defn last-three-ball-positions [data]
+  (take-last 3 (ball-positions data)))
 
