39124b7d7f444aab0c1805c70b9cd3c6f0e879d2
Sauli Tähkäpää <sauli.tahkapaa@netti.fi>
2012-09-23 00:10:46 +0300

    Fixed logging path.

--- start-duel.sh
+++ start-duel.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-mkdir logs -p
-java -jar tor-bjorn.borg-1.0.0-SNAPSHOT-standalone.jar $1 $2 $3 $4 > logs/`date +%F`-`date +%H%M%S`_$1_vs_$2_game.log 2>&1 &
+mkdir log -p
+java -jar tor-bjorn.borg-1.0.0-SNAPSHOT-standalone.jar $1 $2 $3 $4 > log/`date +%F`-`date +%H%M%S`_$1_vs_$2_game.log 2>&1 &
