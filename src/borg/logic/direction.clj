08edddc48ed35746f3b05b1581942872d1a4c6b6
Sauli Tähkäpää <sauli.tahkapaa@netti.fi>
2012-09-26 20:42:20 +0300

    Added rounding to direction.

--- src/borg/logic/direction.clj
+++ src/borg/logic/direction.clj
@@ -1,10 +1,14 @@
 (ns borg.logic.direction
-  (:use borg.constants))
+  (:use borg.constants
+        clojure.contrib.math))
 
 (def min-direction -1.0)
 (def max-direction 1.0)
 (defn distance-to-target-factor [] (/ paddle-height 2)) ;;; smaller factor makes acceleration sharper - larger factor makes it smoother (and longer)
 (defn target-height-offset [] (/ paddle-height 2)) ;;; defines the relative position of the paddle on the target height
+
+(defn- round-with-two-decimals [number]
+ (/ (round (* number 100)) 100.0))
 
 (defn- target-height-with-paddle [target-height]
   (- target-height (target-height-offset)))
@@ -14,6 +18,9 @@
 
 (defn- direction-to-target [target-height current-height]
   (/ (distance-to-target target-height current-height) (distance-to-target-factor)))
+
+(defn- rounded-direction-to-target [target-height current-height]
+  (round-with-two-decimals (direction-to-target target-height current-height)))
 
 (defn- trim-max-direction [direction]
   (if (> direction max-direction)
@@ -26,7 +33,7 @@
     direction))
 
 (defn- trim-direction [target-height current-height]
-  (trim-min-direction (trim-max-direction (direction-to-target target-height current-height))))
+  (trim-min-direction (trim-max-direction (rounded-direction-to-target target-height current-height))))
 
 (defn direction [target-height current-height]
   (trim-direction target-height current-height))
